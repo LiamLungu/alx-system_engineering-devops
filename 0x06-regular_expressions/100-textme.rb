96b8ac29814439455654ad88a4dee80003b80e42
Liam Lungu <Dragoneyeyt2020@gmail.com>
2024-05-07 13:03:21 -0700

    Updated

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,6 @@
-format = ARGV[0].scan(/from:(.\w+)|to:(.\w+)|flags:([0-9:-]+)/)
+#!/usr/bin/env ruby
+
+#format = ARGV[0].scan(/from:(.\w+)|to:(.\w+)|flags:([0-9:-]+)/)
 list = [format[0].compact, format[1].compact, format[2].compact]
 puts list.join(',')
 

100536b1e3bf84fda0ad208bd374a1f2b2d7eb3e
LiamLungu <dragoneyeyt2020@gmail.com>
2024-08-14 01:37:51 -0700

    Done4

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Script that prints out top ten hottest gists
-of a subreddit"""
+"""Script that prints out top ten hottest gists of a subreddit"""
 
 import requests
 
@@ -8,16 +7,24 @@ import requests
 def top_ten(subreddit):
     """Function that fetches top 10 gists"""
     apiUrl = "https://reddit.com/r/{}/hot.json".format(subreddit)
-    userAgent = "Mozilla/5.0"
+    userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     limits = 10
 
     response = requests.get(
-        apiUrl, headers={"user-agent": userAgent}, params={"limit": limits})
-    if not response:
+        apiUrl, headers={"user-agent": userAgent}, params={"limit": limits}, allow_redirects=False)
+    
+    if response.status_code != 200:
         print('None')
         return
-    response = response.json()
-    list_obj = response['data']['children']
-    for obj in list_obj:
-        print(obj['data']['title'])
-    return
+    
+    try:
+        response = response.json()
+        list_obj = response['data']['children']
+        for obj in list_obj[:limits]:  # Ensures only top 10 are printed
+            print(obj['data']['title'])
+    except Exception as e:
+        print('None')
+
+
+# Example usage
+top_ten('python')
